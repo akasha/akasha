c3d1ec5770bed3720ea5357fd8dc987234d4a57a
Peter Donald <peter@realityforge.org>
2020-03-15 20:40:05 +1100

    Remove idlHeader that appears in several specs

--- src/main/java/org/realityforge/webtack/FetchCommand.java
+++ src/main/java/org/realityforge/webtack/FetchCommand.java
@@ -197,6 +197,10 @@ final class FetchCommand
 
         final Document document = Jsoup.parse( input.toFile(), StandardCharsets.UTF_8.name(), url );
         final Elements elements = document.select( CSS_SELECTOR );
+
+        // Some spec docs (gamepad, push_api, screen_orientation, selection_api) include an anchor in block so strip it out
+        elements.forEach( e -> e.getElementsByClass( "idlHeader" ).forEach( Node::remove ) );
+
         final String idl = elements
           .stream()
           // Not part of an example block
