619524bb9b534fa9119f3625a3a85d8464ff80f4
Peter Donald <peter@realityforge.org>
2020-03-21 14:59:16 +1100

    Placeholder failing test

---
+++ webidl/src/test/java/org/realityforge/webtack/model/ExtendedAttributeTest.java
@@ -0,0 +1,13 @@
+package org.realityforge.webtack.model;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public final class ExtendedAttributeTest
+{
+  @Test
+  public void writeTest()
+  {
+    fail();
+  }
+}
