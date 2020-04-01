735e25f9bc4c264db21cab50af0c4c8f5437e7d5
Peter Donald <peter@realityforge.org>
2020-04-02 09:48:46 +1100

    Mark class as final

--- webidl-model/src/test/java/org/realityforge/webtack/model/ArgumentTest.java
+++ webidl-model/src/test/java/org/realityforge/webtack/model/ArgumentTest.java
@@ -6,7 +6,7 @@ import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-public class ArgumentTest
+public final class ArgumentTest
   extends AbstractTest
 {
   @Test
