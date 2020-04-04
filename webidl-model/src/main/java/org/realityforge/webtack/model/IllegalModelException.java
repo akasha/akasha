62d0366a544ba6b63e20e8e394ec3c302c2a26e8
Peter Donald <peter@realityforge.org>
2020-04-04 14:06:22 +1100

    Remove unused constructors

--- webidl-model/src/main/java/org/realityforge/webtack/model/IllegalModelException.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/IllegalModelException.java
@@ -5,22 +5,8 @@ import javax.annotation.Nullable;
 public class IllegalModelException
   extends RuntimeException
 {
-  public IllegalModelException()
-  {
-  }
-
   public IllegalModelException( @Nullable final String message )
   {
     super( message );
-  }
-
-  public IllegalModelException( @Nullable final String message, @Nullable final Throwable cause )
-  {
-    super( message, cause );
-  }
-
-  public IllegalModelException( @Nullable final Throwable cause )
-  {
-    super( cause );
   }
 }
