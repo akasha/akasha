7a6f152f2455a35b3244f76d7874a1f0672bc074
Peter Donald <peter@realityforge.org>
2020-04-09 15:15:03 +1000

    Reorganize test

--- webidl-model/src/test/java/org/realityforge/webtack/model/OperationMemberTest.java
+++ webidl-model/src/test/java/org/realityforge/webtack/model/OperationMemberTest.java
@@ -1,10 +1,7 @@
 package org.realityforge.webtack.model;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -15,66 +12,54 @@ public final class OperationMemberTest
   public void parse()
     throws Exception
   {
-    {
-      final OperationMember operationMember =
-        ensureOperationMember( "Promise<boolean> isSessionSupported(XRSessionMode mode);",
-                               "isSessionSupported",
-                               Kind.Promise );
-      assertEquals( operationMember.getKind(), OperationMember.Kind.OPERATOR );
-      assertEquals( ( (PromiseType) operationMember.getReturnType() ).getResolveType().getKind(), Kind.Boolean );
-      final List<Argument> arguments = operationMember.getArguments();
-      assertEquals( arguments.size(), 1 );
-      final Argument argument1 = arguments.get( 0 );
-      assertEquals( argument1.getName(), "mode" );
-      final Type argument1Type = argument1.getType();
-      assertEquals( argument1Type.getKind(), Kind.TypeReference );
-      assertEquals( ( (TypeReference) argument1Type ).getName(), "XRSessionMode" );
-    }
-
-    {
-      final OperationMember operationMember =
-        ensureOperationMember( "getter XRInputSource(unsigned long index);",
-                               null,
-                               Kind.TypeReference );
-      assertEquals( operationMember.getKind(), OperationMember.Kind.GETTER );
-      assertEquals( ( (TypeReference) operationMember.getReturnType() ).getName(), "XRInputSource" );
-      final List<Argument> arguments = operationMember.getArguments();
-      assertEquals( arguments.size(), 1 );
-      final Argument argument1 = arguments.get( 0 );
-      assertEquals( argument1.getName(), "index" );
-      assertEquals( argument1.getType().getKind(), Kind.UnsignedLong );
-    }
-
-    {
-      final OperationMember operationMember =
-        ensureOperationMember( "boolean includes(any key);",
-                               "includes",
-                               Kind.Boolean );
-      assertEquals( operationMember.getKind(), OperationMember.Kind.OPERATOR );
-      final List<Argument> arguments = operationMember.getArguments();
-      assertEquals( arguments.size(), 1 );
-      final Argument argument1 = arguments.get( 0 );
-      assertEquals( argument1.getName(), "key" );
-      assertEquals( argument1.getType().getKind(), Kind.Any );
-    }
+    final OperationMember member =
+      WebIDLModelParser.parse( createParser( "Promise<boolean> isSessionSupported(XRSessionMode mode);" ).operation(),
+                               Collections.emptyList() );
+    assertEquals( member.getName(), "isSessionSupported" );
+    assertEquals( member.getReturnType().getKind(), Kind.Promise );
+    assertEquals( member.getKind(), OperationMember.Kind.OPERATOR );
+    assertEquals( ( (PromiseType) member.getReturnType() ).getResolveType().getKind(), Kind.Boolean );
+    final List<Argument> arguments = member.getArguments();
+    assertEquals( arguments.size(), 1 );
+    final Argument argument1 = arguments.get( 0 );
+    assertEquals( argument1.getName(), "mode" );
+    final Type argument1Type = argument1.getType();
+    assertEquals( argument1Type.getKind(), Kind.TypeReference );
+    assertEquals( ( (TypeReference) argument1Type ).getName(), "XRSessionMode" );
   }
 
-  @Nonnull
-  private OperationMember ensureOperationMember( @Nonnull final String idl,
-                                                 @Nullable final String name,
-                                                 @Nonnull final Kind returnKind )
-    throws IOException
+  @Test
+  public void parse_getter()
+    throws Exception
   {
-    final OperationMember member = parse( idl );
-    assertEquals( member.getName(), name );
-    assertEquals( member.getReturnType().getKind(), returnKind );
-    return member;
+    final OperationMember member =
+      WebIDLModelParser.parse( createParser( "getter XRInputSource(unsigned long index);" ).operation(),
+                               Collections.emptyList() );
+    assertNull( member.getName() );
+    assertEquals( member.getReturnType().getKind(), Kind.TypeReference );
+    assertEquals( member.getKind(), OperationMember.Kind.GETTER );
+    assertEquals( ( (TypeReference) member.getReturnType() ).getName(), "XRInputSource" );
+    final List<Argument> arguments = member.getArguments();
+    assertEquals( arguments.size(), 1 );
+    final Argument argument1 = arguments.get( 0 );
+    assertEquals( argument1.getName(), "index" );
+    assertEquals( argument1.getType().getKind(), Kind.UnsignedLong );
+
   }
 
-  @Nonnull
-  private OperationMember parse( @Nonnull final String webIDL )
-    throws IOException
+  @Test
+  public void parse_specialName()
+    throws Exception
   {
-    return WebIDLModelParser.parse( createParser( webIDL ).operation(), Collections.emptyList() );
+    final OperationMember member =
+      WebIDLModelParser.parse( createParser( "boolean includes(any key);" ).operation(), Collections.emptyList() );
+    assertEquals( member.getName(), "includes" );
+    assertEquals( member.getReturnType().getKind(), Kind.Boolean );
+    assertEquals( member.getKind(), OperationMember.Kind.OPERATOR );
+    final List<Argument> arguments = member.getArguments();
+    assertEquals( arguments.size(), 1 );
+    final Argument argument1 = arguments.get( 0 );
+    assertEquals( argument1.getName(), "key" );
+    assertEquals( argument1.getType().getKind(), Kind.Any );
   }
 }
