61d095c6530dc2b8cad1fafee2b9de3eac93e466
Peter Donald <peter@realityforge.org>
2020-04-29 21:02:41 +1000

    Remove unused code

--- cli/src/main/java/org/realityforge/webtack/CountingConsoleErrorListener.java
+++ cli/src/main/java/org/realityforge/webtack/CountingConsoleErrorListener.java
@@ -1,14 +1,10 @@
 package org.realityforge.webtack;
 
-import java.util.BitSet;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import org.antlr.v4.runtime.ConsoleErrorListener;
-import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
 
 final class CountingConsoleErrorListener
   extends ConsoleErrorListener
@@ -19,23 +15,10 @@ final class CountingConsoleErrorListener
   @Nonnull
   private final String _sourceName;
   private int _errorCount;
-  private int _ambiguityCount;
 
   CountingConsoleErrorListener( @Nonnull final String sourceName )
   {
     _sourceName = Objects.requireNonNull( sourceName );
-  }
-
-  @Override
-  public void reportAmbiguity( final Parser recognizer,
-                               final DFA dfa,
-                               final int startIndex,
-                               final int stopIndex,
-                               final boolean exact,
-                               final BitSet ambigAlts,
-                               final ATNConfigSet configs )
-  {
-    _ambiguityCount++;
   }
 
   @Override
