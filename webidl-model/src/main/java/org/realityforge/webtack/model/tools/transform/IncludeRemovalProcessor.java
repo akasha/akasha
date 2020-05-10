2753cd14b8857c255b70f76149caa279f621c691
Peter Donald <peter@realityforge.org>
2020-05-10 20:22:42 +1000

    Add processor for removing matching includes

---
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/transform/IncludeRemovalProcessor.java
@@ -0,0 +1,38 @@
+package org.realityforge.webtack.model.tools.transform;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.realityforge.webtack.model.IncludesStatement;
+
+/**
+ * Remove includes that match a pattern.
+ */
+public final class IncludeRemovalProcessor
+  extends AbstractSchemaProcessor
+{
+  @Nonnull
+  private final Pattern _interfacePattern;
+  @Nonnull
+  private final Pattern _mixinPattern;
+
+  public IncludeRemovalProcessor( @Nonnull final Pattern interfacePattern, @Nonnull final Pattern mixinPattern )
+  {
+    _interfacePattern = Objects.requireNonNull( interfacePattern );
+    _mixinPattern = Objects.requireNonNull( mixinPattern );
+  }
+
+  @Nullable
+  @Override
+  protected IncludesStatement transformIncludesStatement( @Nonnull final IncludesStatement input )
+  {
+    return matches( input ) ? null : super.transformIncludesStatement( input );
+  }
+
+  private boolean matches( @Nonnull final IncludesStatement input )
+  {
+    return _interfacePattern.matcher( input.getInterfaceName() ).matches() &&
+           _mixinPattern.matcher( input.getMixinName() ).matches();
+  }
+}
