208c933d1ccafb90eabeb1c1f178f3ddb9fdee9e
Peter Donald <peter@realityforge.org>
2020-06-24 21:16:04 +1000

    Merge all generator specific code

--- jsinterop-generator/src/main/java/org/realityforge/webtack/jsinterop/CallbackInterfaceGenerator.java
+++ jsinterop-generator/src/main/java/org/realityforge/webtack/jsinterop/CallbackInterfaceGenerator.java
@@ -1,38 +1,65 @@
 package org.realityforge.webtack.jsinterop;
 
 import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.lang.model.element.Modifier;
 import org.realityforge.webtack.model.Argument;
 import org.realityforge.webtack.model.CallbackInterfaceDefinition;
 import org.realityforge.webtack.model.ConstMember;
 import org.realityforge.webtack.model.ConstValue;
+import org.realityforge.webtack.model.Element;
 import org.realityforge.webtack.model.EnumerationDefinition;
+import org.realityforge.webtack.model.ExtendedAttribute;
 import org.realityforge.webtack.model.InterfaceDefinition;
 import org.realityforge.webtack.model.Kind;
 import org.realityforge.webtack.model.OperationMember;
+import org.realityforge.webtack.model.SequenceType;
 import org.realityforge.webtack.model.Type;
 import org.realityforge.webtack.model.TypeReference;
+import org.realityforge.webtack.model.TypedefDefinition;
+import org.realityforge.webtack.model.WebIDLSchema;
 
 final class CallbackInterfaceGenerator
 {
+  void generate( @Nonnull final CodeGenContext context )
+    throws IOException
+  {
+    final WebIDLSchema schema = context.getSchema();
+    for ( final CallbackInterfaceDefinition definition : schema.getCallbackInterfaces() )
+    {
+      generate( context, definition );
+    }
+    for ( final EnumerationDefinition definition : schema.getEnumerations() )
+    {
+      generate( context, definition );
+    }
+    for ( final InterfaceDefinition definition : schema.getInterfaces() )
+    {
+      generate( context, definition );
+    }
+  }
+
   void generate( @Nonnull final CodeGenContext context, @Nonnull final CallbackInterfaceDefinition definition )
     throws IOException
   {
-    final boolean exposedOnGlobal = ElementUtil.isExposedOnGlobal( definition );
+    final boolean exposedOnGlobal = isExposedOnGlobal( definition );
     final TypeSpec.Builder type =
       TypeSpec
         .interfaceBuilder( definition.getName() )
         .addModifiers( Modifier.PUBLIC );
-    CodeGenUtil.writeGeneratedAnnotation( type );
+    writeGeneratedAnnotation( type );
     type.addAnnotation( AnnotationSpec.builder( Types.JS_TYPE )
                           .addMember( "isNative", "true" )
                           .addMember( "namespace", "$T.GLOBAL", Types.JS_PACKAGE )
@@ -44,13 +71,13 @@ final class CallbackInterfaceGenerator
 
     generateDefaultOperation( context, definition.getOperation(), true, type );
 
-    CodeGenUtil.writeTopLevelType( context, type );
+    context.writeTopLevelType( type );
   }
 
   void generate( @Nonnull final CodeGenContext context, @Nonnull final InterfaceDefinition definition )
     throws IOException
   {
-    final boolean exposedOnGlobal = ElementUtil.isExposedOnGlobal( definition );
+    final boolean exposedOnGlobal = isExposedOnGlobal( definition );
     if ( !exposedOnGlobal )
     {
       throw new UnsupportedOperationException( "Unexpected scenario with interface named '" + definition.getName() +
@@ -60,7 +87,7 @@ final class CallbackInterfaceGenerator
       TypeSpec
         .classBuilder( definition.getName() )
         .addModifiers( Modifier.PUBLIC );
-    CodeGenUtil.writeGeneratedAnnotation( type );
+    writeGeneratedAnnotation( type );
     type.addAnnotation( AnnotationSpec.builder( Types.JS_TYPE )
                           .addMember( "isNative", "true" )
                           .addMember( "namespace", "$T.GLOBAL", Types.JS_PACKAGE )
@@ -93,7 +120,7 @@ final class CallbackInterfaceGenerator
       }
     }
 
-    CodeGenUtil.writeTopLevelType( context, type );
+    context.writeTopLevelType( type );
   }
 
   private void generateConstants( @Nonnull final CodeGenContext context,
@@ -111,9 +138,9 @@ final class CallbackInterfaceGenerator
                                  @Nonnull final TypeSpec.Builder type )
   {
     final Type constantType = constant.getType();
-    final Type actualType = CodeGenUtil.resolveTypeDefs( context, constantType );
+    final Type actualType = resolveTypeDefs( context, constantType );
     final FieldSpec.Builder field =
-      FieldSpec.builder( CodeGenUtil.toTypeName( context, constantType, actualType ),
+      FieldSpec.builder( toTypeName( context, constantType, actualType ),
                          constant.getName(),
                          Modifier.PUBLIC,
                          Modifier.STATIC,
@@ -198,8 +225,8 @@ final class CallbackInterfaceGenerator
     final Type returnType = operation.getReturnType();
     if ( Kind.Void != returnType.getKind() )
     {
-      final Type actualType = CodeGenUtil.resolveTypeDefs( context, returnType );
-      if ( CodeGenUtil.isNullable( context, returnType ) )
+      final Type actualType = resolveTypeDefs( context, returnType );
+      if ( isNullable( context, returnType ) )
       {
         method.addAnnotation( Types.NULLABLE );
       }
@@ -207,7 +234,7 @@ final class CallbackInterfaceGenerator
       {
         method.addAnnotation( Types.NONNULL );
       }
-      method.returns( CodeGenUtil.toTypeName( context, returnType, actualType ) );
+      method.returns( toTypeName( context, returnType, actualType ) );
     }
     for ( int i = 0; i < maxArgumentCount; i++ )
     {
@@ -260,15 +287,15 @@ final class CallbackInterfaceGenerator
                                           @Nonnull final MethodSpec.Builder method )
   {
     final Type argumentType = argument.getType();
-    final Type actualType = CodeGenUtil.resolveTypeDefs( context, argumentType );
+    final Type actualType = resolveTypeDefs( context, argumentType );
     final ParameterSpec.Builder parameter =
-      ParameterSpec.builder( CodeGenUtil.toTypeName( context, argumentType, actualType ), argument.getName() );
+      ParameterSpec.builder( toTypeName( context, argumentType, actualType ), argument.getName() );
     addMagicConstantAnnotationIfNeeded( context, actualType, parameter );
     if ( isFinal )
     {
       parameter.addModifiers( Modifier.FINAL );
     }
-    if ( CodeGenUtil.isNullable( context, argumentType ) )
+    if ( isNullable( context, argumentType ) )
     {
       parameter.addAnnotation( Types.NULLABLE );
     }
@@ -310,7 +337,7 @@ final class CallbackInterfaceGenerator
       TypeSpec
         .classBuilder( definition.getName() )
         .addModifiers( Modifier.PUBLIC, Modifier.FINAL );
-    CodeGenUtil.writeGeneratedAnnotation( type );
+    writeGeneratedAnnotation( type );
 
     for ( final String value : definition.getValues() )
     {
@@ -327,7 +354,7 @@ final class CallbackInterfaceGenerator
 
     type.addMethod( MethodSpec.constructorBuilder().addModifiers( Modifier.PRIVATE ).build() );
 
-    CodeGenUtil.writeTopLevelType( context, type );
+    context.writeTopLevelType( type );
   }
 
   @Nonnull
@@ -353,5 +380,243 @@ final class CallbackInterfaceGenerator
   boolean addMagicConstantsAnnotation()
   {
     return true;
+  }
+
+  private void writeGeneratedAnnotation( @Nonnull final TypeSpec.Builder builder )
+  {
+    Class<?> generated = getGeneratedAnnotation();
+    if ( null != generated )
+    {
+      builder.addAnnotation( AnnotationSpec.builder( ClassName.get( generated ) )
+                               .addMember( "value", "$S", "org.realityforge.webtack" )
+                               .build() );
+    }
+  }
+
+  @Nullable
+  private Class<?> getGeneratedAnnotation()
+  {
+    try
+    {
+      return Class.forName( "javax.annotation.processing.Generated" );
+    }
+    catch ( final ClassNotFoundException ignored )
+    {
+      try
+      {
+        return Class.forName( "javax.annotation.Generated" );
+      }
+      catch ( final ClassNotFoundException ignored2 )
+      {
+        //Generate no annotation
+        return null;
+      }
+    }
+  }
+
+  private boolean isNullable( @Nonnull final CodeGenContext context, @Nonnull final Type type )
+  {
+    if ( type.isNullable() )
+    {
+      return true;
+    }
+    else if ( Kind.TypeReference == type.getKind() )
+    {
+      final String name = ( (TypeReference) type ).getName();
+      final TypedefDefinition typedef = context.getSchema().findTypedefByName( name );
+      if ( null != typedef )
+      {
+        return isNullable( context, typedef.getType() );
+      }
+    }
+    return false;
+  }
+
+  @Nonnull
+  private Type resolveTypeDefs( @Nonnull final CodeGenContext context, @Nonnull final Type type )
+  {
+    if ( Kind.TypeReference == type.getKind() )
+    {
+      final String name = ( (TypeReference) type ).getName();
+      final TypedefDefinition typedef = context.getSchema().findTypedefByName( name );
+      if ( null != typedef )
+      {
+        return resolveTypeDefs( context, typedef.getType() );
+      }
+    }
+    return type;
+  }
+
+  @Nonnull
+  private TypeName toTypeName( @Nonnull final CodeGenContext context,
+                               @Nonnull final Type type,
+                               @Nonnull final Type actualType )
+  {
+    final Kind kind = type.getKind();
+
+    final boolean nullable = type.isNullable();
+    if ( nullable &&
+         ( Kind.Byte == kind ||
+           Kind.Octet == kind ||
+           Kind.Short == kind ||
+           Kind.UnsignedShort == kind ||
+           Kind.Long == kind ||
+           Kind.LongLong == kind ||
+           Kind.UnsignedLongLong == kind ) )
+    {
+      throw new UnsupportedOperationException( "Nullable " + kind + " not supported" );
+    }
+
+    if ( Kind.Any == kind )
+    {
+      return Types.ANY;
+    }
+    else if ( Kind.Void == kind )
+    {
+      return TypeName.VOID;
+    }
+    else if ( Kind.Boolean == kind )
+    {
+      return nullable ? TypeName.BOOLEAN.box() : TypeName.BOOLEAN;
+    }
+    else if ( Kind.Byte == kind )
+    {
+      return TypeName.BYTE;
+    }
+    else if ( Kind.Octet == kind )
+    {
+      return TypeName.SHORT;
+    }
+    else if ( Kind.UnsignedShort == kind )
+    {
+      return TypeName.INT;
+    }
+    else if ( Kind.Long == kind )
+    {
+      return TypeName.INT;
+    }
+    else if ( Kind.UnsignedLong == kind )
+    {
+      // UnsignedLong is not representable in a JVM but we may it using a signed integer when in jsinterop
+      // and just hope it produces the correct value.
+      return TypeName.INT;
+    }
+    else if ( Kind.LongLong == kind )
+    {
+      // LongLong is actually the same size as a java long in the jre but the way that
+      // it is transpiled by GWT/J2CL means we need to represent it as an integer but
+      // acknowledge that at runtime the value can exceed what the java type represents
+      return TypeName.INT;
+    }
+    else if ( Kind.UnsignedLongLong == kind )
+    {
+      // Not representable natively in java but in jsinterop it is best represented as an integer
+      // See comment on LongLong type
+      return TypeName.INT;
+    }
+    else if ( Kind.Float == kind || Kind.UnrestrictedFloat == kind )
+    {
+      return nullable ? TypeName.DOUBLE.box() : TypeName.FLOAT;
+    }
+    else if ( Kind.Double == kind || Kind.UnrestrictedDouble == kind )
+    {
+      return nullable ? TypeName.DOUBLE.box() : TypeName.DOUBLE;
+    }
+    else if ( Kind.DOMString == kind || Kind.ByteString == kind || Kind.USVString == kind )
+    {
+      return Types.STRING;
+    }
+    else if ( Kind.Object == kind )
+    {
+      return TypeName.OBJECT;
+    }
+    else if ( Kind.Symbol == kind )
+    {
+      return Types.SYMBOL;
+    }
+    else if ( Kind.TypeReference == kind )
+    {
+      final TypeReference typeReference = (TypeReference) type;
+      final String name = typeReference.getName();
+      final TypedefDefinition typedef = context.getSchema().findTypedefByName( name );
+      if ( null != typedef )
+      {
+        return toTypeName( context, actualType, actualType );
+      }
+      else
+      {
+        return context.lookupTypeByName( name );
+      }
+    }
+    else if ( Kind.Promise == kind )
+    {
+      return Types.PROMISE;
+    }
+    else if ( Kind.Sequence == kind )
+    {
+      final Type itemType = ( (SequenceType) type ).getItemType();
+      return ParameterizedTypeName.get( Types.JS_ARRAY,
+                                        toTypeName( context, itemType, resolveTypeDefs( context, itemType ) ) );
+    }
+    else if ( Kind.Record == kind )
+    {
+      throw new UnsupportedOperationException( "Record not currently supported by generator" );
+    }
+    else if ( Kind.ArrayBuffer == kind )
+    {
+      return Types.ARRAY_BUFFER;
+    }
+    else if ( Kind.DataView == kind )
+    {
+      return Types.DATA_VIEW;
+    }
+    else if ( Kind.Int8Array == kind )
+    {
+      return Types.INT8_ARRAY;
+    }
+    else if ( Kind.Int16Array == kind )
+    {
+      return Types.INT16_ARRAY;
+    }
+    else if ( Kind.Int32Array == kind )
+    {
+      return Types.INT32_ARRAY;
+    }
+    else if ( Kind.Uint8Array == kind )
+    {
+      return Types.UINT8_ARRAY;
+    }
+    else if ( Kind.Uint16Array == kind )
+    {
+      return Types.UINT16_ARRAY;
+    }
+    else if ( Kind.Uint32Array == kind )
+    {
+      return Types.UINT32_ARRAY;
+    }
+    else if ( Kind.Uint8ClampedArray == kind )
+    {
+      return Types.UINT8_CLAMPED_ARRAY;
+    }
+    else if ( Kind.Float32Array == kind )
+    {
+      return Types.FLOAT32_ARRAY;
+    }
+    else if ( Kind.Float64Array == kind )
+    {
+      return Types.FLOAT64_ARRAY;
+    }
+    else
+    {
+      throw new UnsupportedOperationException( kind + " type not currently supported by generator" );
+    }
+  }
+
+  private boolean isExposedOnGlobal( @Nonnull final Element element )
+  {
+    return element.getExtendedAttributes()
+      .stream()
+      .filter( a -> a.getKind() == ExtendedAttribute.Kind.IDENT || a.getKind() == ExtendedAttribute.Kind.IDENT_LIST )
+      .anyMatch( a -> a.getName().equals( "Exposed" ) );
   }
 }
