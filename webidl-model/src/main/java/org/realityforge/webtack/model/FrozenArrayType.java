e64e491743dddad7c302549b37d03a0cacec70d8
Peter Donald <peter@realityforge.org>
2020-06-30 23:24:11 +1000

    Make public to allow transformation

--- webidl-model/src/main/java/org/realityforge/webtack/model/FrozenArrayType.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/FrozenArrayType.java
@@ -10,10 +10,10 @@ public final class FrozenArrayType
   @Nonnull
   private final Type _itemType;
 
-  FrozenArrayType( @Nonnull final Type itemType,
-                   @Nonnull final List<ExtendedAttribute> extendedAttributes,
-                   final boolean nullable,
-                   @Nonnull final List<SourceInterval> sourceLocations )
+  public FrozenArrayType( @Nonnull final Type itemType,
+                          @Nonnull final List<ExtendedAttribute> extendedAttributes,
+                          final boolean nullable,
+                          @Nonnull final List<SourceInterval> sourceLocations )
   {
     super( Kind.FrozenArray, extendedAttributes, nullable, sourceLocations );
     _itemType = Objects.requireNonNull( itemType );
