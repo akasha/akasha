b3c099f66a10c8a5ba3337c8c0bf066318ac2ae7
Peter Donald <peter@realityforge.org>
2020-07-15 16:40:17 +1000

    Read enum value as interface

--- webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/remove_element/ElementType.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/remove_element/ElementType.java
@@ -1,11 +1,14 @@
 package org.realityforge.webtack.model.tools.processors.remove_element;
 
+import javax.json.bind.annotation.JsonbProperty;
+
 public enum ElementType
 {
   callback,
   callback_interface,
   dictionary,
   enumeration,
+  @JsonbProperty( "interface" )
   interface_type,
   mixin,
   namespace
