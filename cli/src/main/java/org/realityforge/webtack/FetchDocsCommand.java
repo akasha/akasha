067b0655b939617821141daf6f29cf78378e0da9
Peter Donald <peter@realityforge.org>
2020-08-01 22:55:05 +1000

    Fetch docs is done via doc source names rather idl source names

--- cli/src/main/java/org/realityforge/webtack/FetchDocsCommand.java
+++ cli/src/main/java/org/realityforge/webtack/FetchDocsCommand.java
@@ -1,10 +1,5 @@
 package org.realityforge.webtack;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,16 +9,12 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import org.realityforge.getopt4j.CLOption;
 import org.realityforge.getopt4j.CLOptionDescriptor;
-import org.realityforge.webtack.model.InterfaceDefinition;
-import org.realityforge.webtack.model.WebIDLModelParser;
-import org.realityforge.webtack.model.WebIDLSchema;
 import org.realityforge.webtack.model.tools.mdn_scanner.DocException;
 import org.realityforge.webtack.model.tools.mdn_scanner.DocKind;
 import org.realityforge.webtack.model.tools.mdn_scanner.MdnDocScanner;
 import org.realityforge.webtack.model.tools.mdn_scanner.SourceFetchException;
 import org.realityforge.webtack.model.tools.mdn_scanner.SourceIOException;
-import org.realityforge.webtack.model.tools.repository.config.RepositoryConfig;
-import org.realityforge.webtack.model.tools.repository.config.SourceConfig;
+import org.realityforge.webtack.model.tools.repository.config.DocSourceConfig;
 
 final class FetchDocsCommand
   extends ConfigurableCommand
@@ -37,20 +28,20 @@ final class FetchDocsCommand
       new CLOptionDescriptor( "force",
                               CLOptionDescriptor.ARGUMENT_DISALLOWED,
                               FORCE_OPT,
-                              "Force the downloading of the source even if the last modified at time indicates it is up to date." ),
+                              "Force the downloading of the documentation even if the last modified at time indicates it is up to date." ),
       new CLOptionDescriptor( "no-remove-source",
                               CLOptionDescriptor.ARGUMENT_DISALLOWED,
                               NO_REMOVE_SOURCE_OPT,
-                              "Do not remove source after fetching and extracting documentation." )
+                              "Do not remove html after fetching and extracting documentation." )
     };
   @Nonnull
-  private final Set<String> _sourceNames = new LinkedHashSet<>();
+  private final Set<String> _typeNames = new LinkedHashSet<>();
   private boolean _force;
   private boolean _noRemoveSource;
 
   FetchDocsCommand()
   {
-    super( COMMAND, "Fetch the documentation for WebIDL sources", OPTIONS );
+    super( COMMAND, "Fetch the documentation for WebIDL types", OPTIONS );
   }
 
   @Override
@@ -70,7 +61,7 @@ final class FetchDocsCommand
       else
       {
         assert CLOption.TEXT_ARGUMENT == optionId;
-        _sourceNames.add( option.getArgument() );
+        _typeNames.add( option.getArgument() );
       }
     }
 
@@ -81,55 +72,39 @@ final class FetchDocsCommand
   int run( @Nonnull final Context context )
   {
     final Logger logger = context.environment().logger();
-    final RepositoryConfig config = context.config();
+    final MdnDocScanner scanner =
+      new MdnDocScanner( context.docRepository(), context.environment().docDirectory() );
 
-    for ( final String sourceName : getSourceNames( config ) )
+    for ( final String typeName : getTypeNames( context ) )
     {
       if ( logger.isLoggable( Level.FINE ) )
       {
-        logger.log( Level.FINE, "Processing source named '" + sourceName + "'" );
+        logger.log( Level.FINE, "Fetched documentation for element named '" + typeName + "'" );
       }
-      final SourceConfig source = config.findSourceByName( sourceName );
-      if ( null == source )
+
+      try
+      {
+        scanner.fetchDocumentation( DocKind.Type, typeName, null, _force, !_noRemoveSource );
+      }
+      catch ( final SourceFetchException sfe )
       {
         final String message =
-          "Error: Attempting to process source with the name '" + sourceName + "' but no such source exists.";
-        throw new TerminalStateException( message, ExitCodes.ERROR_SOURCE_DOES_NOT_EXIST_CODE );
+          "Error: Failed to fetch documentation for type named '" + sfe.getSource().getName() +
+          "' with the error: " + sfe.getCause();
+        throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_FETCH_FAILED_CODE );
       }
-      if ( logger.isLoggable( Level.FINE ) )
+      catch ( final SourceIOException sioe )
       {
-        logger.log( Level.FINE, "Fetched documentation for source named '" + sourceName + "'" );
+        final String message =
+          "Error: " + sioe.getMessage() + ". Element: " + sioe.getSource().getName() +
+          " Error: " + sioe.getCause();
+        throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_IO_ERROR_CODE );
       }
-      final WebIDLSchema schema = loadSchema( context, source );
-      final MdnDocScanner scanner =
-        new MdnDocScanner( context.docRepository(), context.environment().docDirectory() );
-
-      for ( final InterfaceDefinition definition : schema.getInterfaces() )
+      catch ( final DocException sioe )
       {
-        try
-        {
-          scanner.fetchDocumentation( DocKind.Type, definition.getName(), null, _force, !_noRemoveSource );
-        }
-        catch ( final SourceFetchException sfe )
-        {
-          final String message =
-            "Error: Failed to fetch documentation for element named '" + sfe.getSource().getName() +
-            "' with the error: " + sfe.getCause();
-          throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_FETCH_FAILED_CODE );
-        }
-        catch ( final SourceIOException sioe )
-        {
-          final String message =
-            "Error: " + sioe.getMessage() + ". Element: " + sioe.getSource().getName() +
-            " Error: " + sioe.getCause();
-          throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_IO_ERROR_CODE );
-        }
-        catch ( final DocException sioe )
-        {
-          final String message =
-            "Error: Unexpected error fetching documentation. Error: " + sioe.getCause();
-          throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_UNEXPECTED_ERROR_CODE );
-        }
+        final String message =
+          "Error: Unexpected error fetching documentation. Error: " + sioe.getCause();
+        throw new TerminalStateException( message, ExitCodes.ERROR_DOC_SOURCE_UNEXPECTED_ERROR_CODE );
       }
     }
 
@@ -137,65 +112,11 @@ final class FetchDocsCommand
   }
 
   @Nonnull
-  private WebIDLSchema loadSchema( @Nonnull final Context context, @Nonnull final SourceConfig source )
+  private Set<String> getTypeNames( @Nonnull final Context context )
   {
-    final Path target = context.environment().getPathForSource( source );
-    if ( !Files.exists( target ) )
-    {
-      final String message =
-        "Error: IDL source with the name '" + source.getName() + "' has not been fetched. " +
-        "The tool can not fetch documentation until the IDL has been fetched.";
-      throw new TerminalStateException( message, ExitCodes.ERROR_SOURCE_NOT_FETCHED_CODE );
-    }
 
-    final Logger logger = context.environment().logger();
-    try ( final FileReader reader = new FileReader( target.toFile() ) )
-    {
-      final CountingConsoleErrorListener errorListener = new CountingConsoleErrorListener( target.toString() );
-      final WebIDLSchema schema =
-        WebIDLModelParser.parse( target.toString(), Collections.emptySet(), reader, errorListener );
-
-      final int errorCount = errorListener.getErrorCount();
-      if ( 0 == errorCount )
-      {
-        if ( logger.isLoggable( Level.FINE ) )
-        {
-          logger.log( Level.FINE, "Source named '" + source.getName() + "' parsed without errors." );
-        }
-        return schema;
-      }
-      else
-      {
-        final String message =
-          "Error: Attempting to parse source named '" + source.getName() + "' but there was " + errorCount +
-          " errors detected in the WebIDL";
-        throw new TerminalStateException( message, ExitCodes.ERROR_IDL_NOT_VALID_CODE );
-      }
-    }
-    catch ( final IOException ioe )
-    {
-      final String message =
-        "Error: Attempting to parse source named '" + source.getName() + "' but there was an error " +
-        "reading the WebIDL for source. Error: " + ioe;
-      throw new TerminalStateException( message, ExitCodes.ERROR_SOURCE_NOT_FETCHED_CODE );
-    }
-    catch ( final Throwable t )
-    {
-      final String message =
-        "Error: Attempting to parse source with the name '" + source.getName() +
-        "' but there was an unexpected error verifying source. Error: " + t;
-      throw new TerminalStateException( message, ExitCodes.ERROR_SOURCE_NOT_FETCHED_CODE );
-    }
-  }
-
-  @Nonnull
-  private Set<String> getSourceNames( @Nonnull final RepositoryConfig config )
-  {
-    return _sourceNames.isEmpty() ?
-           config.getSources()
-             .stream()
-             .map( SourceConfig::getName )
-             .collect( Collectors.toCollection( LinkedHashSet::new ) ) :
-           _sourceNames;
+    return _typeNames.isEmpty() ?
+           context.docRepository().getSources().stream().map( DocSourceConfig::getName ).collect( Collectors.toSet() ) :
+           _typeNames;
   }
 }
