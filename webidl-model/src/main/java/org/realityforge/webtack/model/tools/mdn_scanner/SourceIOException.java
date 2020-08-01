f794ff1920024bd4f34df25cbd78958cd6fd74f2
Peter Donald <peter@realityforge.org>
2020-08-01 14:36:37 +1000

    Rename exception

--- webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/SourceIOException.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/SourceIOException.java
@@ -4,15 +4,15 @@ import java.util.Objects;
 import javax.annotation.Nonnull;
 import org.realityforge.webtack.model.tools.repository.config.DocSourceConfig;
 
-public final class SourceWriteException
+public final class SourceIOException
   extends DocException
 {
   @Nonnull
   private final DocSourceConfig _source;
 
-  public SourceWriteException( @Nonnull final DocSourceConfig source,
-                               @Nonnull final String message,
-                               @Nonnull final Throwable cause )
+  public SourceIOException( @Nonnull final DocSourceConfig source,
+                            @Nonnull final String message,
+                            @Nonnull final Throwable cause )
   {
     super( message, cause );
     _source = Objects.requireNonNull( source );
