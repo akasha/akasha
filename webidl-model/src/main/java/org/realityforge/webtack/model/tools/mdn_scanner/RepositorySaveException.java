759d41de2ffbc97b4b0569ecb181ed361f1a95fc
Peter Donald <peter@realityforge.org>
2020-08-01 13:48:23 +1000

    Add exception resulting from error saving repository

---
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/RepositorySaveException.java
@@ -0,0 +1,24 @@
+package org.realityforge.webtack.model.tools.mdn_scanner;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.realityforge.webtack.model.tools.repository.config.DocRepositoryConfig;
+
+public final class RepositorySaveException
+  extends DocException
+{
+  @Nonnull
+  private final DocRepositoryConfig _repository;
+
+  public RepositorySaveException( @Nonnull final DocRepositoryConfig repository, @Nonnull final Throwable cause )
+  {
+    super( cause );
+    _repository = Objects.requireNonNull( repository );
+  }
+
+  @Nonnull
+  public DocRepositoryConfig getRepository()
+  {
+    return _repository;
+  }
+}
