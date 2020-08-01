e189fe84397717746bff87707c4eeece5e7340ec
Peter Donald <peter@realityforge.org>
2020-08-01 13:45:10 +1000

    Add base exception for all problems related to fetching, processing and saving documentation

---
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/DocException.java
@@ -0,0 +1,9 @@
+package org.realityforge.webtack.model.tools.mdn_scanner;
+
+/**
+ * Base exception for all errors that occur fetching, processing and saving documentation for web apis.
+ */
+public abstract class DocException
+  extends Exception
+{
+}
