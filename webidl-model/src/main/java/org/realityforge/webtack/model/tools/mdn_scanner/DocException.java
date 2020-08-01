fed6273cd4980a533fb70327b9741b4d0b1c3812
Peter Donald <peter@realityforge.org>
2020-08-01 13:46:38 +1000

    Add constructors useful for subclasses

--- webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/DocException.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/mdn_scanner/DocException.java
@@ -1,9 +1,29 @@
 package org.realityforge.webtack.model.tools.mdn_scanner;
 
+import javax.annotation.Nullable;
+
 /**
  * Base exception for all errors that occur fetching, processing and saving documentation for web apis.
  */
 public abstract class DocException
   extends Exception
 {
+  public DocException()
+  {
+  }
+
+  public DocException( @Nullable final String message )
+  {
+    super( message );
+  }
+
+  public DocException( @Nullable final String message, @Nullable final Throwable cause )
+  {
+    super( message, cause );
+  }
+
+  public DocException( @Nullable final Throwable cause )
+  {
+    super( cause );
+  }
 }
