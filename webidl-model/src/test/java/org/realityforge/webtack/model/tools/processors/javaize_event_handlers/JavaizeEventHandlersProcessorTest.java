31daf6191b03d03baf613e060d8f1bac70f669b0
Peter Donald <peter@realityforge.org>
2020-08-06 08:49:56 +1000

    Make sure we load required doc repository config

--- webidl-model/src/test/java/org/realityforge/webtack/model/tools/processors/javaize_event_handlers/JavaizeEventHandlersProcessorTest.java
+++ webidl-model/src/test/java/org/realityforge/webtack/model/tools/processors/javaize_event_handlers/JavaizeEventHandlersProcessorTest.java
@@ -1,7 +1,9 @@
 package org.realityforge.webtack.model.tools.processors.javaize_event_handlers;
 
+import java.nio.file.Path;
 import javax.annotation.Nonnull;
 import javax.json.Json;
+import org.realityforge.webtack.model.tools.mdn_scanner.config.DocRepositoryConfig;
 import org.realityforge.webtack.model.tools.processors.AbstractProcessorTest;
 import org.realityforge.webtack.model.tools.spi.Processor;
 import org.realityforge.webtack.model.tools.spi.Registry;
@@ -15,19 +17,25 @@ public final class JavaizeEventHandlersProcessorTest
   public void registry()
   {
     assertTrue( Registry.isProcessorPresent( "JavaizeEventHandlers" ) );
-    assertNotNull( createProcessor() );
+    assertNotNull( createProcessor( "basic" ) );
   }
 
   @Test
   public void basic()
     throws Exception
   {
-    performStandardFixtureTest( "basic", this::createProcessor );
+    performStandardFixtureTest( "basic", () -> createProcessor( "basic" ) );
   }
 
+  @SuppressWarnings( "SameParameterValue" )
   @Nonnull
-  private Processor createProcessor()
+  private Processor createProcessor( @Nonnull final String subDirectory )
   {
-    return Registry.createProcessor( "JavaizeEventHandlers", Json.createObjectBuilder().build() );
+    final Path configFile = getTestLocalFixtureDir().resolve( subDirectory ).resolve( DocRepositoryConfig.FILENAME );
+    return Registry.createProcessor( "JavaizeEventHandlers",
+                                     Json
+                                       .createObjectBuilder()
+                                       .add( "docsRepositoryConfigFile", configFile.toString() )
+                                       .build() );
   }
 }
