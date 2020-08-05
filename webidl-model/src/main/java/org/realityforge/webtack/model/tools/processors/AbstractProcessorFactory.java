d8a7380c9e2f99f1594f16d22a98d2997e460757
Peter Donald <peter@realityforge.org>
2020-08-06 08:00:49 +1000

    Extract helper method

--- webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/AbstractProcessorFactory.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/AbstractProcessorFactory.java
@@ -1,10 +1,15 @@
 package org.realityforge.webtack.model.tools.processors;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.realityforge.webtack.model.ExtendedAttribute;
+import org.realityforge.webtack.model.tools.mdn_scanner.DocRepositoryRuntime;
+import org.realityforge.webtack.model.tools.mdn_scanner.config.DocRepositoryConfig;
 import org.realityforge.webtack.model.tools.spi.ProcessorFactory;
 
 public abstract class AbstractProcessorFactory
@@ -48,6 +53,33 @@ public abstract class AbstractProcessorFactory
     {
       throw new IllegalArgumentException( getProcessorName() + " supplied invalid configuration value " + key +
                                           " that is not a valid java regular expression", pse );
+    }
+  }
+
+  @SuppressWarnings( "SameParameterValue" )
+  @Nonnull
+  protected final DocRepositoryRuntime requireDocRepository( @Nonnull final String key, @Nonnull final String value )
+  {
+    final String filename = requireNonNull( key, value );
+    final Path path = Paths.get( "." ).resolve( filename );
+    if ( !Files.exists( path ) )
+    {
+      throw new IllegalArgumentException( getProcessorName() + " supplied " + key + " configuration value " +
+                                          value + " but no such file exists at " + path );
+    }
+    return new DocRepositoryRuntime( loadDocRepositoryConfig( path ), path.getParent().resolve( "docs" ) );
+  }
+
+  @Nonnull
+  private DocRepositoryConfig loadDocRepositoryConfig( @Nonnull final Path path )
+  {
+    try
+    {
+      return DocRepositoryConfig.load( path );
+    }
+    catch ( final Exception e )
+    {
+      throw new IllegalArgumentException( getProcessorName() + " failed to load doc repository from " + path, e );
     }
   }
 
--- webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/merge_docs/MergeDocsProcessorFactory.java
+++ webidl-model/src/main/java/org/realityforge/webtack/model/tools/processors/merge_docs/MergeDocsProcessorFactory.java
@@ -20,28 +20,6 @@ public final class MergeDocsProcessorFactory
   @Override
   public Processor create()
   {
-    final String filename = requireNonNull( "docsRepositoryConfigFile", docsRepositoryConfigFile );
-    final Path path = Paths.get( "." ).resolve( filename );
-    if ( !Files.exists( path ) )
-    {
-      throw new IllegalArgumentException( getProcessorName() + " supplied docsRepositoryConfigFile configuration " +
-                                          "value " + docsRepositoryConfigFile + " but no such file exists at " + path );
-    }
-    final DocRepositoryRuntime runtime =
-      new DocRepositoryRuntime( loadDocRepositoryConfig( path ), path.getParent().resolve( "docs" ) );
-    return new MergeDocsProcessor( runtime );
-  }
-
-  @Nonnull
-  private DocRepositoryConfig loadDocRepositoryConfig( @Nonnull final Path path )
-  {
-    try
-    {
-      return DocRepositoryConfig.load( path );
-    }
-    catch ( final Exception e )
-    {
-      throw new IllegalArgumentException( getProcessorName() + " failed to load doc repository from " + path, e );
-    }
+    return new MergeDocsProcessor( requireDocRepository( "docsRepositoryConfigFile", docsRepositoryConfigFile ) );
   }
 }
