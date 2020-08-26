48a0d87099224059c6ab50d6ff54699aa12adc7e
Peter Donald <peter@realityforge.org>
2020-08-26 22:58:55 +1000

    Add placeholder code for react4j generator

---
+++ react4j-generator/src/test/java/org/realityforge/webtack/react4j/JavaProcess.java
@@ -0,0 +1,36 @@
+package org.realityforge.webtack.react4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+final class JavaProcess
+{
+  private JavaProcess()
+  {
+  }
+
+  @SuppressWarnings( "SameParameterValue" )
+  static int exec( @Nonnull final String className,
+                   @Nonnull final List<String> jvmArgs,
+                   @Nonnull final List<String> args,
+                   @Nonnull final List<String> classpath )
+    throws IOException, InterruptedException
+  {
+    final String javaHome = System.getProperty( "java.home" );
+    final String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+    final List<String> command = new ArrayList<>();
+    command.add( javaBin );
+    command.addAll( jvmArgs );
+    command.add( "-cp" );
+    command.add( String.join( File.pathSeparator, classpath ) );
+    command.add( className );
+    command.addAll( args );
+    final ProcessBuilder builder = new ProcessBuilder( command );
+    final Process process = builder.inheritIO().start();
+    process.waitFor();
+    return process.exitValue();
+  }
+}
