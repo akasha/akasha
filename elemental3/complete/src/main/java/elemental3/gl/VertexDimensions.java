efdcee4d23c495cd6c2d433115cce94cdf96ee93
Peter Donald <peter@realityforge.org>
2020-10-16 09:00:06 +1100

    Lock down vertext dimensions

---
+++ elemental3/complete/src/main/java/elemental3/gl/VertexDimensions.java
@@ -0,0 +1,14 @@
+package elemental3.gl;
+
+import java.lang.annotation.Documented;
+import org.intellij.lang.annotations.Language;
+import org.intellij.lang.annotations.MagicConstant;
+
+/**
+ * Specifies the number of number of components per vertex attribute.
+ */
+@Documented
+@MagicConstant( intValues = { 1, 2, 3, 4 } )
+public @interface VertexDimensions
+{
+}
