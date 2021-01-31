e516926885ed37c5329a3c5a35ab12e61319256a
Peter Donald <peter@realityforge.org>
2021-01-31 16:32:11 +1100

    Validate values

--- elemental3/complete/src/main/java/elemental3/gl/VertexDimensions.java
+++ elemental3/complete/src/main/java/elemental3/gl/VertexDimensions.java
@@ -10,4 +10,20 @@ import org.intellij.lang.annotations.MagicConstant;
 @MagicConstant( intValues = { 1, 2, 3, 4 } )
 public @interface VertexDimensions
 {
+  final class Validator
+  {
+    private Validator()
+    {
+    }
+
+    public static void assertValid( final int value )
+    {
+      assert isValid( value );
+    }
+
+    public static boolean isValid( final int value )
+    {
+      return 1 == value || 2 == value || 3 == value || 4 == value;
+    }
+  }
 }
