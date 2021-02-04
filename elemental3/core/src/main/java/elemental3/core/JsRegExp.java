c67efa998ca2013f81cf8353301ea47b16fc64d5
Peter Donald <peter@realityforge.org>
2021-02-04 19:55:01 +1100

    Remove some deprecated features

--- elemental3/core/src/main/java/elemental3/core/JsRegExp.java
+++ elemental3/core/src/main/java/elemental3/core/JsRegExp.java
@@ -7,20 +7,6 @@ import jsinterop.annotations.JsType;
 @JsType( name = "RegExp", isNative = true, namespace = JsPackage.GLOBAL )
 public class JsRegExp
 {
-  public static String $1;
-  public static String $2;
-  public static String $3;
-  public static String $4;
-  public static String $5;
-  public static String $6;
-  public static String $7;
-  public static String $8;
-  public static String $9;
-  public static String input;
-  public static String lastMatch;
-  public static String lastParen;
-  public static String leftContext;
-  public static String rightContext;
   public String flags;
   public boolean global;
   public boolean ignoreCase;
@@ -40,12 +26,6 @@ public class JsRegExp
   public JsRegExp( Object pattern )
   {
   }
-
-  @Deprecated
-  public native void compile( String pattern, Object flags );
-
-  @Deprecated
-  public native void compile( String pattern );
 
   public native RegExpResult exec( String str );
 
