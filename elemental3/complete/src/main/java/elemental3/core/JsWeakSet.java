e2a4b708def9cbf7ad7eda2e80587720dbed945c
Peter Donald <peter@realityforge.org>
2021-02-05 09:59:56 +1100

    Remove unnecessary constructors

--- elemental3/complete/src/main/java/elemental3/core/JsWeakSet.java
+++ elemental3/complete/src/main/java/elemental3/core/JsWeakSet.java
@@ -1,50 +1,12 @@
 package elemental3.core;
 
-import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
-import jsinterop.base.Js;
 
 @JsType( name = "WeakSet", isNative = true, namespace = JsPackage.GLOBAL )
 public class JsWeakSet<VALUE>
 {
-  @JsType( isNative = true, name = "?", namespace = JsPackage.GLOBAL )
-  public interface ConstructorIterableUnionType<VALUE>
-  {
-    @JsOverlay
-    static <T> ConstructorIterableUnionType<T> of( Object o )
-    {
-      return Js.cast( o );
-    }
-
-    @JsOverlay
-    default JsArray<VALUE> asJsArray()
-    {
-      return Js.cast( this );
-    }
-
-    @JsOverlay
-    default JsIterable<VALUE> asJsIterable()
-    {
-      return Js.cast( this );
-    }
-
-    @JsOverlay
-    default boolean isJsArray()
-    {
-      return this instanceof JsArray;
-    }
-  }
-
   public JsWeakSet()
-  {
-  }
-
-  public JsWeakSet( ConstructorIterableUnionType<VALUE> iterable )
-  {
-  }
-
-  public JsWeakSet( JsArray<VALUE> iterable )
   {
   }
 
