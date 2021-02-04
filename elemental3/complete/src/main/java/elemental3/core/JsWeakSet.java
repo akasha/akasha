129868bf92328ef09065625dd1228d3b30776b1d
Peter Donald <peter@realityforge.org>
2021-02-05 10:09:44 +1100

    Nullability annotations

--- elemental3/complete/src/main/java/elemental3/core/JsWeakSet.java
+++ elemental3/complete/src/main/java/elemental3/core/JsWeakSet.java
@@ -1,5 +1,7 @@
 package elemental3.core;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
@@ -10,19 +12,20 @@ public class JsWeakSet<VALUE>
   {
   }
 
-  public JsWeakSet( JsIterable<VALUE> iterable )
+  public JsWeakSet( @Nonnull JsIterable<VALUE> iterable )
   {
   }
 
-  public JsWeakSet( VALUE[] iterable )
+  public JsWeakSet( @Nonnull VALUE[] iterable )
   {
   }
 
+  @Nonnull
   public native JsWeakSet<VALUE> add( VALUE value );
 
   public native void clear();
 
-  public native boolean delete( VALUE value );
+  public native boolean delete( @Nonnull VALUE value );
 
-  public native boolean has( VALUE value );
+  public native boolean has( @Nonnull VALUE value );
 }
