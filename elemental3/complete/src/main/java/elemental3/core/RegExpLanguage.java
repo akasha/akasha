d930974f08068380b1be3b8214613e253fdf9dc8
Peter Donald <peter@realityforge.org>
2021-02-04 22:49:19 +1100

    Add annotations that can be used to provide IDE support for JS RegExp fields

---
+++ elemental3/complete/src/main/java/elemental3/core/RegExpLanguage.java
@@ -0,0 +1,14 @@
+package elemental3.core;
+
+import java.lang.annotation.Documented;
+import org.intellij.lang.annotations.Language;
+import org.intellij.lang.annotations.Pattern;
+
+/**
+ * Specifies the valid flag string for regexps.
+ */
+@Documented
+@Language( "JSRegexp" )
+public @interface RegExpLanguage
+{
+}
