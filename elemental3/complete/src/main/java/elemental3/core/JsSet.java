28b5c694870682ca0c447a1c56b230b7363246d3
Peter Donald <peter@realityforge.org>
2021-02-05 17:05:48 +1100

    Remove unnecessary constructor

--- elemental3/complete/src/main/java/elemental3/core/JsSet.java
+++ elemental3/complete/src/main/java/elemental3/core/JsSet.java
@@ -1,43 +1,13 @@
 package elemental3.core;
 
 import jsinterop.annotations.JsFunction;
-import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
-import jsinterop.base.Js;
 
 @JsType( name = "Set", isNative = true, namespace = JsPackage.GLOBAL )
 public class JsSet<VALUE>
   implements JsIterable<VALUE>
 {
-  @JsType( isNative = true, name = "?", namespace = JsPackage.GLOBAL )
-  public interface ConstructorIterableUnionType<VALUE>
-  {
-    @JsOverlay
-    static <T> ConstructorIterableUnionType<T> of( Object o )
-    {
-      return Js.cast( o );
-    }
-
-    @JsOverlay
-    default JsArray<VALUE> asJsArray()
-    {
-      return Js.cast( this );
-    }
-
-    @JsOverlay
-    default JsIterable<VALUE> asJsIterable()
-    {
-      return Js.cast( this );
-    }
-
-    @JsOverlay
-    default boolean isJsArray()
-    {
-      return this instanceof JsArray;
-    }
-  }
-
   @JsFunction
   public interface ForEachCallbackFn<VALUE>
   {
@@ -47,14 +17,6 @@ public class JsSet<VALUE>
   public int size;
 
   public JsSet()
-  {
-  }
-
-  public JsSet( ConstructorIterableUnionType<VALUE> iterable )
-  {
-  }
-
-  public JsSet( JsArray<VALUE> iterable )
   {
   }
 
