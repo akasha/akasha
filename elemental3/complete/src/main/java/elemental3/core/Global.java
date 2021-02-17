6668bb188b3c29e1087c34c0e7c54ca3340f05a4
Peter Donald <peter@realityforge.org>
2021-02-18 09:23:14 +1100

    Remove constants already available from java.lang.Double

--- elemental3/complete/src/main/java/elemental3/core/Global.java
+++ elemental3/complete/src/main/java/elemental3/core/Global.java
@@ -1,15 +1,10 @@
 package elemental3.core;
 
-import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
 @JsType( isNative = true, name = "goog.global", namespace = JsPackage.GLOBAL )
 public class Global
 {
-  @JsOverlay
-  public static final double Infinity = Global__Constants.Infinity;
-  @JsOverlay
-  public static final double NaN = Global__Constants.NaN;
   public static Arguments arguments;
 }
