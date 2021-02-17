53c08ef735b656b19ed55d407cc3e3e9d1c0ad39
Peter Donald <peter@realityforge.org>
2021-02-18 09:11:32 +1100

    Remove methods exposed on generated global object

--- elemental3/complete/src/main/java/elemental3/core/Global.java
+++ elemental3/complete/src/main/java/elemental3/core/Global.java
@@ -14,26 +14,4 @@ public class Global
   public static Arguments arguments;
   @JsOverlay
   public static final Object undefined = Global__Constants.undefined;
-
-  public static native String decodeURI( String uri );
-
-  public static native String decodeURIComponent( String uri );
-
-  public static native String encodeURI( String uri );
-
-  public static native String encodeURIComponent( String uri );
-
-  public static native String escape( String str );
-
-  public static native Object eval( String code );
-
-  public static native boolean isFinite( Object num );
-
-  public static native boolean isNaN( Object num );
-
-  public static native double parseFloat( Object num );
-
-  public static native int parseInt( Object num, int base );
-
-  public static native String unescape( String str );
 }
