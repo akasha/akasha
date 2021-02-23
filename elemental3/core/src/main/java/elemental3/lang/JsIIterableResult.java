a050c57fbda3e3e2c61db9cd566edc72af9c76e5
Peter Donald <peter@realityforge.org>
2021-02-23 14:21:20 +1100

    Remove factory method never expected to be invoked

--- elemental3/core/src/main/java/elemental3/lang/JsIIterableResult.java
+++ elemental3/core/src/main/java/elemental3/lang/JsIIterableResult.java
@@ -1,21 +1,12 @@
 package elemental3.lang;
 
-import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
-import jsinterop.base.Js;
-import jsinterop.base.JsPropertyMap;
 
 @JsType( isNative = true, name = "IIterableResult", namespace = JsPackage.GLOBAL )
 public interface JsIIterableResult<T>
 {
-  @JsOverlay
-  static <T> JsIIterableResult<T> create()
-  {
-    return Js.uncheckedCast( JsPropertyMap.of() );
-  }
-
   @JsProperty
   T getValue();
 
