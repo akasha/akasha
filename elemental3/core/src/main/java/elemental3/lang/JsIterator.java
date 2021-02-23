ab108885c40c5d3d07fe076aafe52347f72148b8
Peter Donald <peter@realityforge.org>
2021-02-23 15:24:03 +1100

    Remove explicit typing

--- elemental3/core/src/main/java/elemental3/lang/JsIterator.java
+++ elemental3/core/src/main/java/elemental3/lang/JsIterator.java
@@ -3,7 +3,7 @@ package elemental3.lang;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType( isNative = true, name = "Iterator", namespace = JsPackage.GLOBAL )
+@JsType( isNative = true, name = "?", namespace = JsPackage.GLOBAL )
 public interface JsIterator<T>
 {
   JsIIterableResult<T> next();
