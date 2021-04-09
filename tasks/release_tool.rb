c37428cbb8abc690cf1a9b5305fca8ba0018c3e1
Peter Donald <peter@realityforge.org>
2021-04-09 12:02:38 +1000

    Update the release_tool source code.

--- tasks/release_tool.rb
+++ tasks/release_tool.rb
@@ -39,20 +39,14 @@ module Buildr
       end
 
       def derive_versions_from_changelog(options = {})
-        next_version_action = options[:next_version_action]
-        changelog = IO.read('CHANGELOG.md')
-        ENV['PREVIOUS_PRODUCT_VERSION'] ||= changelog[/^### \[v(\d+\.\d+(\.\d+)?)\]/, 1] || '0.00'
+        ENV['PREVIOUS_PRODUCT_VERSION'] ||= IO.read('CHANGELOG.md')[/^### \[v(\d+\.\d+(\.\d+)?)\]/, 1] || '0.00'
+        ENV['PRODUCT_VERSION'] ||= derive_next_version(ENV['PREVIOUS_PRODUCT_VERSION'], options)
+      end
 
-        next_version = ENV['PRODUCT_VERSION']
-        unless next_version
-          if next_version_action
-            next_version = next_version_action.call
-          else
-            version_parts = ENV['PREVIOUS_PRODUCT_VERSION'].split('.')
-            next_version = "#{version_parts[0]}.#{sprintf('%02d', version_parts[1].to_i + 1)}#{version_parts.length > 2 ? ".#{version_parts[2]}" : ''}"
-          end
-          ENV['PRODUCT_VERSION'] = next_version
-        end
+      def derive_next_version(current_version, options = {})
+        return options[:next_version_action].call(current_version) if options[:next_version_action]
+        version_parts = current_version.split('.')
+        next_version = "#{version_parts[0]}.#{sprintf('%02d', version_parts[1].to_i + 1)}#{version_parts.length > 2 ? ".#{version_parts[2]}" : ''}"
       end
 
       private
