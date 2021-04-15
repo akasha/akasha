c8fff8fa9f4a0f7a518b549c95e17c3dc73ea84f
Peter Donald <peter@realityforge.org>
2021-04-15 11:06:08 +1000

    Add some manual patches to closure externs that are required due to the types required in the base closure library.

---
+++ akasha/java/src/main/java/akasha/akasha_patches.extern.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview
+ * @externs
+ */
+
+/**
+ * This type def is an alias because the type is used in closure/base that is added during J2CL compiles.
+ * @typedef {Document}
+ */
+var HTMLDocument;
+
+/**
+ * This type def is an alias because the type is used in closure/base that is added during J2CL compiles.
+ * @typedef {string}
+ */
+var TrustedScript;
