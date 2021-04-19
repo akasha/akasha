75c3a938e7a5126fb97fa79326a4ed5c66a7c5ff
Peter Donald <peter@realityforge.org>
2021-04-20 08:47:43 +1000

    Add some typing for promise.any

--- akasha/java/src/main/java/akasha/akasha_patches.extern.js
+++ akasha/java/src/main/java/akasha/akasha_patches.extern.js
@@ -14,3 +14,21 @@ var HTMLDocument;
  * @typedef {string}
  */
 var TrustedScript;
+
+/**
+ * This does not yet have a typing declared in closure compiler.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ * @param {!Iterable<VALUE>} iterable
+ * @return {!Promise<RESULT>}
+ * @template VALUE
+ * @template RESULT := mapunion(VALUE, (V) =>
+ *     cond(isUnknown(V),
+ *         unknown(),
+ *         cond(isTemplatized(V) && sub(rawTypeOf(V), 'IThenable'),
+ *             templateTypeOf(V, 0),
+ *             cond(sub(V, 'Thenable'), unknown(), V))))
+ * =:
+ */
+Promise.any = function(iterable) {};
+
