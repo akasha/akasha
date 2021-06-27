314b9aa29c8baee3df351cfa0d33b5eba555d24a
Peter Donald <peter@realityforge.org>
2021-06-28 09:56:06 +1000

    Add @WGSL annotation to indicate sublanguage is used for particular values

---
+++ akasha/java/src/main/java/akasha/gpu/WGSL.java
@@ -0,0 +1,13 @@
+package akasha.gpu;
+
+import java.lang.annotation.Documented;
+import org.intellij.lang.annotations.Language;
+
+/**
+ * Specifies that an element of the program represents a WGSL snippet.
+ */
+@Documented
+@Language( "WGSL" )
+public @interface WGSL
+{
+}
