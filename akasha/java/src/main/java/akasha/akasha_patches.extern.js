ad0b68e491dfca98d87203e496831ec9aca7f574
Peter Donald <peter@realityforge.org>
2021-08-07 08:07:31 +1000

    Move externs for elements defined in akasha.core into a separate externs file in the associated package

--- akasha/java/src/main/java/akasha/akasha_patches.extern.js
+++ akasha/java/src/main/java/akasha/akasha_patches.extern.js
@@ -11,46 +11,7 @@
 var HTMLDocument;
 
 /**
- * This type def is an alias because the type defined in the closure compiler externs does not match the underlying symbol name.
- *
- * @typedef {JSONType}
- */
-var JSON;
-
-/**
  * This type def is an alias because the type is used in closure/base that is added during J2CL compiles.
  * @typedef {string}
  */
 var TrustedScript;
-
-/**
- * This does not yet have a typing declared in closure compiler.
- * NOTE: This has been submitted upstream and should be removed once it is accepted.
- *
- * @const {symbol}
- */
-Symbol.split;
-
-/**
- * This does not yet have a typing declared in closure compiler.
- * NOTE: This has been submitted upstream and should be removed once it is accepted.
- *
- * @type {boolean}
- */
-RegExp.prototype.unicode;
-
-/**
- * This does not yet have a typing declared in closure compiler.
- * NOTE: This has been submitted upstream and should be removed once it is accepted.
- *
- * @type {boolean}
- */
-RegExp.prototype.dotAll;
-
-/**
- * Typedef for type used in jsinterop but associated with JSON type which has no externs generated as it is part of the
- * core types.
- *
- * @typedef {(string|number)}
- */
-var StringOrLongLongUnion;
