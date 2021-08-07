51177b9bc79764868fcab1862e2259b0852bf309
Peter Donald <peter@realityforge.org>
2021-08-07 16:27:34 +1000

    Add predefined javascript to externs work

---
+++ webtack/jsinterop-generator/src/test/fixtures/js_predefined_symbols/input/main/js/com/example/predefiend.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview
+ * @externs
+ */
+/*
+ *
+ *
+ * Everything below is is "hand-written" and marked as predefined and thus should not cause duplicate errors
+ *
+ *
+ */
+
+/**
+ * @typedef {(!Int32Array|!Array<!number>)}
+ */
+var Int32List2;
+/**
+ * @typedef {(!Int32Array|!Array<!number>)}
+ */
+var NumbersList;
+/**
+ * @typedef {{jsElapsedTime:(!number|undefined),propertyName:(!string|undefined),propertyName2:(!string|undefined)}}
+ */
+var EventInit;
+/**
+ * @const
+ */
+var JsMath;
+/** @const {!number} */ JsMath.JsE;
+/** @const {!number} */ JsMath.E2;
+/** @const {!number} */ JsMath.JsPI;
+/** @const {!number} */ JsMath.PI2;
+/**
+ * @constructor
+ * @private
+ * @nosideeffects
+ */
+function Thing2() {}
+/** @type {!Object} */ Thing2.staticReadonlyObjectValue2;
+/** @type {!Object} */ Thing2.staticObjectValue;
+/**
+ * @return {!Object}
+ */
+Thing2.staticMethod2 = function() {}
+/**
+ * @constructor
+ * @private
+ * @nosideeffects
+ */
+function Element() {}
+/** @type {!number} */ Thing.prototype.zeSize;
+/** @type {!number} */ Thing.prototype.length2;
+/**
+ * @param {!number} i
+ * @return {?Element}
+ */
+Thing.prototype.leItem = function(i) {}
+/**
+ * @param {!number} index
+ * @return {?Element}
+ */
+Thing.prototype.item2 = function(index) {}
