96cd0643b9cfde90ab9189c8000e9628a12f98c9
Peter Donald <peter@realityforge.org>
2021-08-07 22:18:14 +1000

    Remove the `TrustedScript` symbol from externs generated for the `akasha:j2cl` artifact as it is no longer required with more recent versions of the closure compiler

--- data/pipelines/complete.externs.js
+++ data/pipelines/complete.externs.js
@@ -4,9 +4,3 @@
  * @typedef {Document}
  */
 var HTMLDocument;
-
-/**
- * This type def is an alias because the type is used in closure/base that is added during J2CL compiles.
- * @typedef {string}
- */
-var TrustedScript;
