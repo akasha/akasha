4854aed4584e16123c0a0cb135d07f10b78a9ee7
Peter Donald <peter@realityforge.org>
2021-10-22 10:53:12 +1100

    Whitespace

--- akasha/java/src/main/java/akasha/core/JsUtil.java
+++ akasha/java/src/main/java/akasha/core/JsUtil.java
@@ -13,8 +13,8 @@ public final class JsUtil
    * Return the property on the object indexed by the specified symbol.
    *
    * @param object the object.
-   * @param key the symbol to use to access the property.
-   * @param <T> the type of the property.
+   * @param key    the symbol to use to access the property.
+   * @param <T>    the type of the property.
    * @return the property value.
    */
   public static <T> T get( @Nonnull final Object object, @Nonnull final Symbol key )
