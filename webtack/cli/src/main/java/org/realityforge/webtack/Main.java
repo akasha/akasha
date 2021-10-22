439ad5a12748886821fca571c214d47821887272
Peter Donald <peter@realityforge.org>
2021-10-22 11:19:24 +1100

    Code cleanup

--- webtack/cli/src/main/java/org/realityforge/webtack/Main.java
+++ webtack/cli/src/main/java/org/realityforge/webtack/Main.java
@@ -95,7 +95,7 @@ public final class Main
         {
           if ( logger.isLoggable( Level.INFO ) )
           {
-            logger.log( Level.INFO, "Cause: " + cause.toString() );
+            logger.log( Level.INFO, "Cause: " + cause );
             if ( logger.isLoggable( Level.FINE ) )
             {
               logger.log( Level.FINE, null, cause );
