d21c7a2f7f34109947af389eb36a001f1580a40b
Peter Donald <peter@realityforge.org>
2022-02-27 21:57:03 +1100

    Minor grammar improvements

--- akasha/java/src/main/java/akasha/promise/Promise.java
+++ akasha/java/src/main/java/akasha/promise/Promise.java
@@ -38,7 +38,7 @@ public class Promise<T>
   /**
    * The Promise.all() method takes an iterable of promises as an input, and returns a single Promise
    * that resolves to an array of the results of the input promises. This returned promise will resolve
-   * when all of the input's promises have resolved, or if the input iterable contains no promises. It
+   * when all of the input promises have resolved, or if the input iterable contains no promises. It
    * rejects immediately upon any of the input promises rejecting or non-promises throwing an error, and
    * will reject with this first rejection message / error.
    *
@@ -59,7 +59,7 @@ public class Promise<T>
   /**
    * The Promise.all() method takes an iterable of promises as an input, and returns a single Promise
    * that resolves to an array of the results of the input promises. This returned promise will resolve
-   * when all of the input's promises have resolved, or if the input iterable contains no promises. It
+   * when all of the input promises have resolved, or if the input iterable contains no promises. It
    * rejects immediately upon any of the input promises rejecting or non-promises throwing an error, and
    * will reject with this first rejection message / error.
    *
