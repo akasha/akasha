1ef0d50361b183bc967cd20660d79a05908882f0
Peter Donald <peter@realityforge.org>
2022-02-28 19:44:36 +1100

    Improve grammar in javadocs

--- akasha/java/src/main/java/akasha/promise/Promise.java
+++ akasha/java/src/main/java/akasha/promise/Promise.java
@@ -154,7 +154,7 @@ public class Promise<T>
   public static native <V> Promise<V> any( @Nonnull JsIterable<Promise<? extends V>> promises );
 
   /**
-   * The Promise.race() method returns a promise that fulfills or rejects as soon as one of the promises in an iterable fulfills or rejects, with the value or reason from that promise.
+   * The Promise.race() method returns a promise that fulfills or rejects as soon as one of the promises in the iterable fulfills or rejects, with the value or reason from that promise.
    *
    * @param promises the promises.
    * @param <V>      the component type of the promise returned.
@@ -171,7 +171,7 @@ public class Promise<T>
   }
 
   /**
-   * The Promise.race() method returns a promise that fulfills or rejects as soon as one of the promises in an iterable fulfills or rejects, with the value or reason from that promise.
+   * The Promise.race() method returns a promise that fulfills or rejects as soon as one of the promises in the iterable fulfills or rejects, with the value or reason from that promise.
    *
    * @param promises the promises.
    * @param <V>      the component type of the promise returned.
@@ -244,7 +244,7 @@ public class Promise<T>
 
   /**
    * Appends the fulfillment handler to the promise, and returns a new promise resolving to a void value.
-   * This is an affordance added to simplify integration with a java-esque library and is equivalent to a
+   * This is an affordance added to simplify integration with a java-like library and is equivalent to a
    * javascript then method that returns undefined.
    *
    * @param onAccept the callback function called when the promise is fulfilled.
@@ -285,12 +285,12 @@ public class Promise<T>
                                      @Nullable OnRejectedCallback<? extends V> onRejected );
 
   /**
-   * The finally() method returns a Promise. When the promise is settled, i.e either fulfilled or rejected,
+   * The finally() method returns a Promise. When the promise is settled, i.e. either fulfilled or rejected,
    * the specified callback function is executed. This provides a way for code to be run whether the promise
    * was fulfilled successfully or rejected once the Promise has been dealt with.
    *
    * @param onSettle a callback function called when the Promise is settled.
-   * @return a Promise with the finally handler set to the specified callback.
+   * @return a Promise with the "finally" handler set to the specified callback.
    * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally">Promise.finally - MDN</a>
    */
   @JsMethod( name = "finally" )
